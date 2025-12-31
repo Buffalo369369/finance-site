7df6b2bdd1caaf6befb14a22e5b7e66758fbb0a1
Katerina <benthebeaglecto@gmail.com>
2025-12-31 01:26:04 +0100

    update site content and map

--- app/page.tsx
+++ app/page.tsx
@@ -627,13 +627,13 @@ export default function Home() {
 
             <div className="mt-4 overflow-hidden rounded-2xl border border-black/10">
              <iframe
-  src="https://yandex.ru/map-widget/v1/?ll=7.21028%2C51.26064&z=16&l=map&pt=7.21028,51.26064,pm2rdm"
+  src="https://yandex.ru/map-widget/v1/?ll=7.241993%2C51.276360&z=17&l=map&pt=7.241993,51.276360,pm2rdm"
   width="100%"
   height="420"
   frameBorder="0"
   allowFullScreen
   className="rounded-2xl"
-/>
+></iframe>
             </div>
           </div>
         </section>
